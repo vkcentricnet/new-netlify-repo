04814a39698099c2e2309238fe106cb5808cf1e6
Thokozani Mhlongo <133325510+vkcentricnet@users.noreply.github.com>
2023-05-12 13:49:13 +0200

    Hotfix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">Test demo</h1>
     <h3>
-      You’ve successfully created a project with
+      You’ve successfully created a project with 111111 
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
