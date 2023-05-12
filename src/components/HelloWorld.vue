98d2ff7fc0fa0c6cab175ae7eedb0a4bee7c81c0
Thokozani Mhlongo <133325510+vkcentricnet@users.noreply.github.com>
2023-05-12 13:45:28 +0200

    Hogan fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">Test demo</h1>
     <h3>
+      <p>asjhdajhsdjasd</p>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
